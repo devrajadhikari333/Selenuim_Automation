1ac613ec6413992fab5fee503fa7816e0791dca2
“devraj <devrajadhikari333@gmail.com>
2021-05-06 15:12:36 -0500

    added new contents

---
+++ src/gmail_testScripts/verify_gmail_login.java
@@ -0,0 +1,27 @@
+package gmail_testScripts;
+
+
+import gmail_login_page.POM_without_Pagefactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class verify_gmail_login {
+        @Test
+        public void verify_valid_login ()
+        {
+            System.setProperty("webdriver.chrome.driver", "/Users/devrajadhikari/Downloads/chromedriver");
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("http://www.gmail.com");
+
+            POM_without_Pagefactory login = new POM_without_Pagefactory(driver);
+            //login.loginToGmail("devrajadhikari333@gmail.com","123456");
+            login.typeUsername("devrajadhikari333@gmail.com");
+            login.clickUsrNext();
+            login.typePassword("123456");
+            login.clickPassNextButton();
+            driver.quit();
+
+        }
+}
